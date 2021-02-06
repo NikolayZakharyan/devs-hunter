a93764ba0efef8bbee371023e20e7c625b71da59
Nikolay <nikolzakharyan@gmail.com>
2021-02-06 14:56:20 +0400

    search global v3

--- src/components/searchComponent/Search.jsx
+++ src/components/searchComponent/Search.jsx
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import SearchResult from './SearchResult';
 import SearchBar from 'material-ui-search-bar';
 
 const Search = () => {
-  
   const [input, setInput] = useState('');
   const [rearchRes, setRearchRes] = useState(null);
 
   const getInput = () => {
-
     if (input.trim().length) {
-
-
       setRearchRes(input.trim().split(' ').join('+'));
-      
     }
     setInput('');
   };
 
   return (
     <div>
-      <SearchBar
-        value={input}
-        onChange={(e) => setInput(e)}
-        onRequestSearch={() => getInput()}
-      />
+      <div style={{ margin: '0 15%' }}>
+        <SearchBar
+          value={input}
+          onChange={(e) => setInput(e)}
+          onRequestSearch={() => getInput()}
+        />
+      </div>
 
       {rearchRes ? <SearchResult data={rearchRes} /> : <></>}
     </div>
